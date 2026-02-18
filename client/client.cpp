e875d46c26e07e567d79380f41a7d699a24e54c9
HyperBitGore <bruiserjoe01@gmail.com>
2026-02-18 00:44:55 -0600

    basic sockets

--- client/client.cpp
+++ client/client.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <errno.h>
 
-int main () {
-    std::cout << "hello client\n";
+int main() {
+    int client_fd = socket(AF_INET, SOCK_STREAM, 0);
+    
+    struct sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_port = htons(9001);
+    addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+    
+    int result = connect(client_fd, (struct sockaddr*)&addr, sizeof(addr));
+    if (result != 0) {
+        std::cout << "Error connecting to server, error: " << errno << "\n";
+    }
+
+    std::cout << "Connected to server" << std::endl;
+    
+    close(client_fd);
+    
     return 0;
 }
--- server/server.cpp
+++ server/server.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include <errno.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
 
 int main () {
-    std::cout << "hello, server\n";
+    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    sockaddr_in addr = {};
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(9001);
+    
+    int val = bind(server_fd, (struct sockaddr*)&addr, sizeof(addr));
+    if (val != 0) {
+        std::cout << "Failed to bind socket, error: " << errno << "\n";
+        return 1; 
+    }
+    listen(server_fd, 10);
+    
+    std::cout << "Server listening on port 9001" << std::endl;
+    
+    while (true) {
+        int client_fd = accept(server_fd, nullptr, nullptr);
+        std::cout << "Client connected" << std::endl;
+        
+        close(client_fd);
+    }
+    
     return 0;
 }
